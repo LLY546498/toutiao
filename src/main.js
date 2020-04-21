01d0474148d88dc0ec5217954f2bafcbf9db7c00
LLY546498 <820954981@qq.com>
2020-04-21 11:02:26 +0800

    引入element组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
+
+// 加载element组件库
+// 加载element组件库的样式
+import 'element-ui/lib/theme-chalk/index.css';
 // 加载全局样式
 import './styles/index.less';
+
+// 全局注册element 组件库
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
